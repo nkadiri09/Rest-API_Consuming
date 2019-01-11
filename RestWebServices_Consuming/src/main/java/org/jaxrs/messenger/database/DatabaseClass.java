0f5b412447a0cb6129b9b00d7e0871fa0732bfe0
nkadiri09 <nkadiri09@gmail.com>
2019-01-10 19:03:58 -0600

    initial commit

---
+++ RestWebServices_Consuming/src/main/java/org/jaxrs/messenger/database/DatabaseClass.java
@@ -0,0 +1,26 @@
+package org.jaxrs.messenger.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jaxrs.messenger.model.Message;
+import org.jaxrs.messenger.model.Profile;
+
+public class DatabaseClass {
+
+	private static Map<Long, Message> messages = new HashMap<>();
+	private static Map<String, Profile> profiles = new HashMap<>();
+
+	
+	public static Map<Long, Message> getMessages() {
+		return messages;
+	}
+	
+	public static Map<String, Profile> getProfiles() {
+		return profiles;
+	}
+
+	
+	
+	
+}
